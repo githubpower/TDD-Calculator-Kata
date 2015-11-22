3b60fd11983c9a86b301cfd0e80894b8df56fb1f
Dotnetswdeveloper <github.power@gmail.com>
2015-11-22 16:31:50 +0000

    Adding first test to check for empty value

---
+++ KataCalculatorTrialRun1.Tests/CalculatorOutputTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace KataCalculatorTrialRun1.Tests
+{
+     [TestFixture]
+    public class CalculatorOutputTests
+    {
+         [Test]
+         public void Add_EmptyValue_OutputZero()
+         {
+             Calculator calculator = new Calculator();
+             Assert.AreEqual(0, calculator.Add(""));
+         }
+    }
+}
+
+
+    
+       
