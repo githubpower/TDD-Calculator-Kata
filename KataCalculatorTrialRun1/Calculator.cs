01b3d79d5f7208396f238d3a1a230b5a9c109d55
Dotnetswdeveloper <github.power@gmail.com>
2015-11-22 21:22:52 +0000

    Added code to pass the custom delimiter test

--- KataCalculatorTrialRun1/Calculator.cs
+++ KataCalculatorTrialRun1/Calculator.cs
@@ -6,11 +6,12 @@ namespace KataCalculatorTrialRun1
 {
     public class Calculator
     {
+        private List<string> delimiterOptions = new List<string>() { ",", "\n" };
         public Calculator()
         {
               
         }
-
+        
         public int Add(string numbers)
         {
             List<int> numbersCollection = new List<int>();
@@ -24,26 +25,46 @@ namespace KataCalculatorTrialRun1
 
         private List<int> convertNumbersToIntCollection(string numbers)
         {
+            
             char defaultInputDelimiter = ',';
             numbers = standardiseToDefaultDelimiter(numbers, defaultInputDelimiter);
             
             List<int> numbersInputted = new List<int>();
             numbersInputted = numbers.Split(defaultInputDelimiter)
                               .Select(x => int.Parse(x)).ToList<int>();
-
             return numbersInputted;
         }
 
+      
         private string standardiseToDefaultDelimiter(string numbers, char defaultDelimiter)
         {
-            string[] delimiterOptions = { ",", "\n" };
-            if (delimiterOptions.Any(numbers.Contains))
-            {
-             numbers =  numbers.Replace("\n", defaultDelimiter.ToString());
+
+            if (numbers.StartsWith("//"))
+            {   char customDelimiter = extractCustomDelimiter(numbers);
+                numbers = removeCustomDelimiterInfoPrepended(numbers);
+                numbers = numbers.Replace(customDelimiter, defaultDelimiter);               
             }
+
+            if (numbers.Contains('\n'))
+            {
+                numbers = numbers.Replace("\n", defaultDelimiter.ToString());
+            }
+          
             return numbers;
-            
         }
+
+        
+        private char extractCustomDelimiter(string numbers)
+        {
+            return Convert.ToChar((numbers.Substring(2, numbers.IndexOf("\n") - 2)));
+        }
+
+        private string removeCustomDelimiterInfoPrepended(string numbers)
+        {
+            numbers = numbers.Remove(0, numbers.IndexOf("\n") + 1);
+            return numbers;
+        }
+
     }
 
     
